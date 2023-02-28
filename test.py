1cae2a325402248863ceba5574c505509f2efa47
kato shigemichi <shigemichi_kato_0501@yahoo.co.jp>
2023-02-28 22:38:06 +0900

    # 変更点 ・新規プロジェクトを立ち上げ

--- test.py
+++ test.py
@@ -4,15 +4,11 @@ import re
 import pprint
 
 REQ_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
-param = {
-    "format": "json",
-    "keyword": "マグカップ",
-    "applicationId": "1041635425516382228"
-}
+param = {"format": "json", "keyword": "マグカップ", "applicationId": "1041635425516382228"}
 
 result = requests.get(REQ_URL, param)
 json_result = result.json()
-for item in json_result['Items']:
+for item in json_result["Items"]:
     # print(re.findall("[0-9]*ml", item["Item"]["itemName"]))
     info = item["Item"]
     image_url = info["mediumImageUrls"]
@@ -20,4 +16,4 @@ for item in json_result['Items']:
     itemUrl = info["itemUrl"]
     itemPrice = info["itemPrice"]
     itemName = info["itemName"]
-    print(itemName,image_url, itemCaption, itemUrl, itemPrice)
+    print(itemName, image_url, itemCaption, itemUrl, itemPrice)
