abf426261589be34fe457bc5135285bab7b6ab35
kato shigemichi <shigemichi_kato_0501@yahoo.co.jp>
2023-03-21 14:42:45 +0900

    # 変更点 ・ソート用の表記を変更するため、urlフィルタを追加 def urladdprice(url): def urladdcapacity(url):

--- main/templatetags/url_filter.py
+++ main/templatetags/url_filter.py
@@ -1,8 +1,18 @@
 from django import template
 register = template.Library()
+import urllib.parse
 
 @register.filter
-def urlFilter(url):
-    if "sort" in url:
-        print(url)
-    return url
+def urladdprice(url):
+    pr = urllib.parse.urlparse(url)
+    d = urllib.parse.parse_qs(pr.query)
+    d["sort"]="price"
+
+    return urllib.parse.urlunparse(pr._replace(query=urllib.parse.urlencode(d, doseq=True)))
+
+@register.filter
+def urladdcapacity(url):
+    pr = urllib.parse.urlparse(url)
+    d = urllib.parse.parse_qs(pr.query)
+    d["sort"]="capacity"
+    return urllib.parse.urlunparse(pr._replace(query=urllib.parse.urlencode(d, doseq=True)))
