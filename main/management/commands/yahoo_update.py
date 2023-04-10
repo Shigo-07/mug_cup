6e7226bec2a973f9e826a4f8e91696dd857e39be
kato shigemichi <shigemichi_kato_0501@yahoo.co.jp>
2023-04-10 23:40:52 +0900

    # 変更点 ・削除用のコマンドを修正

--- main/management/commands/yahoo_update.py
+++ main/management/commands/yahoo_update.py
@@ -4,16 +4,17 @@ import time
 import requests
 from django.conf import settings
 
-REQ_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
-RAKUTEN_ID = settings.RAKUTEN_ID
+REQ_URL = "https://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemLookup"
+YAHOO_ID = settings.YAHOO_ID
+
 
 def RemoveNotExistItem(model):
-    items = model.objects.filter(seller="rakuten")
+    items = model.objects.filter(seller="yahoo")
     delete_counts = 0
     delete_items = []
     for item in items:
         time.sleep(1.5)
-        parameter = {"format": "json", "applicationId": RAKUTEN_ID, "itemCode": item.item_code}
+        parameter = {"appid": YAHOO_ID, "itemcode": item.item_code}
 
         request = requests.get(REQ_URL, parameter)
         if request.status_code != 200:
@@ -22,8 +23,8 @@ def RemoveNotExistItem(model):
 
         json_data = request.json()
         # アイテムが存在しない場合は画像とデータを削除する
-        if len(json_data["Items"]) == 0:
-            delete_items.append(parameter["itemCode"])
+        if int(json_data["ResultSet"]["totalResultsReturned"]) == 0:
+            delete_items.append(parameter["itemcode"])
             item.image.delete()
             item.delete()
             delete_counts += 1
@@ -34,7 +35,7 @@ def RemoveNotExistItem(model):
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("===============start check rakuten item===============")
+        print("===============start check yahoo item===============")
         delete_counts, delete_items = RemoveNotExistItem(Item)
         print(f"削除したデータ数：{delete_counts},itemCode:{'-'.join(delete_items)}")
-        print("===============end check rakuten item===============")
+        print("===============end check yahoo item===============")
